85d44e76863634d3cc1bb47074b1f893818bf3c6
Okan Çifçi <161123817+okancfc@users.noreply.github.com>
2024-02-28 16:03:12 +0300

    musiscreen completed

--- SpotifyClone/SpotifyClone/MenuBar/HomeView/Common/SongView.swift
+++ SpotifyClone/SpotifyClone/MenuBar/HomeView/Common/SongView.swift
@@ -15,22 +15,30 @@ struct SongModel: Hashable {
 
 
 struct SongView: View {
+    @State var isShowing: Bool = false
     var model: SongModel
     var body: some View {
-        VStack(spacing: 10) {
-            Image(model.imageName)
-                .resizable()
-                .aspectRatio(contentMode: .fill)
-                .frame(width: 120, height: 120)
-                .cornerRadius(10)
-            
-            Text(model.title)
-                .foregroundColor(.white)
-                .padding(.horizontal, 10)
-                .font(.footnote)
-                .opacity(0.7)
-                .lineLimit(2)
-        }
-        
+        Button(action: {
+            isShowing = true
+        }, label: {
+            VStack(spacing: 10) {
+                
+                Image(model.imageName)
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 120, height: 120)
+                    .cornerRadius(10)
+                
+                Text(model.title)
+                    .foregroundColor(.white)
+                    .padding(.horizontal, 10)
+                    .font(.footnote)
+                    .opacity(0.7)
+                    .lineLimit(2)
+            }
+        })
+        .fullScreenCover(isPresented: $isShowing, content: {
+            MusicScreen(isShowing: $isShowing, song: model)
+        })
     }
 }
--- SpotifyClone/SpotifyClone/MenuBar/HomeView/MusicScreen/MusicScreen.swift
+++ SpotifyClone/SpotifyClone/MenuBar/HomeView/MusicScreen/MusicScreen.swift
@@ -8,40 +8,53 @@
 import SwiftUI
 
 struct MusicScreen: View {
+    @State var heartButtonIsPressed: Bool = false
+    @State var replayButtonIsPressed: Bool = false
+    @State var hifispeakerButtonIsPressed: Bool = false
+    @State var textBadgeButtonIsPressed: Bool = false
+    @State var shuffleButtonIsPressed: Bool = false
+    @State private var totalSeconds: Double = 0
+    private let maxMinutes: Double = 5
+    private var totalSecondsInMax: Double { maxMinutes * 60 }
+    @Binding var isShowing: Bool
     var song: SongModel
     var body: some View {
         ZStack {
-            LinearGradient(gradient: Gradient(colors: [song.color, .white]), startPoint: .top, endPoint: .bottom)
+            LinearGradient(gradient: Gradient(colors: [song.color, song.color.opacity(0.4)]), startPoint: .top, endPoint: .bottom)
                 .ignoresSafeArea(.all)
             VStack {
                 HStack {
                     Button(action: {
-                        
+                        isShowing = false
                     }, label: {
                         Image(systemName: "chevron.down")
                             .foregroundColor(.white)
-                            .font(.title)
+                            .font(.title3)
+                            .padding(.horizontal, 15)
                     })
                     Spacer()
                     Text(song.title)
                         .foregroundStyle(.white)
-                        .font(.title3)
+                        .font(.title2)
+                        .padding(.horizontal)
                     Spacer()
                     Button(action: {
                         
                     }, label: {
                         Image(systemName: "ellipsis")
                             .foregroundColor(.white)
-                            .font(.title)
+                            .font(.title3)
+                            .padding(.horizontal, 15)
                     })
                 }
                 .padding(.horizontal)
-                .padding()
+                .padding(.top, 60)
+                .padding(.vertical)
                 
                 Image(song.imageName)
                     .resizable()
                     .scaledToFill()
-                    .frame(width: UIScreen.main.bounds.width - 44, height: UIScreen.main.bounds.width - 44)
+                    .frame(width: UIScreen.main.bounds.width - 30, height: UIScreen.main.bounds.width - 30)
                 HStack {
                     VStack(alignment: .leading) {
                         Text(song.title)
@@ -54,18 +67,109 @@ struct MusicScreen: View {
                             .opacity(0.8)
                     }
                     Spacer()
-                    Image(systemName: "heart")
-                        .foregroundColor(.white)
-                        .bold()
-                        .font(.title2)
+                    Button(action: {
+                        heartButtonIsPressed.toggle()
+                    }, label: {
+                        Image(systemName: "heart")
+                            .foregroundColor(heartButtonIsPressed ? Color.green : Color.white)
+                            .font(.title2)
+                    })
                 }
                 .padding(.horizontal, 22)
+                VStack(spacing: -10) {
+                    Slider(value: $totalSeconds, in: 0...totalSecondsInMax)
+                        .padding()
+                        .accentColor(.white)
+                    HStack {
+                        Text("\(formattedTime(second: totalSeconds))")
+                            .bold()
+                            .foregroundColor(.white)
+                            .padding(.horizontal, 22)
+                        Spacer()
+                    }
+                }
+                HStack(spacing: 50) {
+                    
+                    Button(action: {
+                        shuffleButtonIsPressed.toggle()
+                    }, label: {
+                        Image(systemName: "shuffle")
+                            .foregroundColor(shuffleButtonIsPressed ? Color.green : Color.white)
+                            .font(.callout)
+                    })
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "backward.end.fill")
+                            .foregroundColor(.white)
+                            .font(.title2)
+                    })
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "play.circle.fill")
+                            .font(.system(size: 70))
+                            .foregroundColor(.white)
+                            
+                    })
+                    
+                    Button(action: {
+                        
+                    }, label: {
+                        Image(systemName: "forward.end.fill")
+                            .foregroundColor(.white)
+                    })
+                    
+                    Button(action: {
+                        replayButtonIsPressed.toggle()
+                    }, label: {
+                        Image(systemName: "repeat")
+                            .foregroundColor(replayButtonIsPressed ? Color.green : Color.white)
+                            .font(.callout)
+                    })
+                }
+                .padding()
+               Spacer()
                 
+                HStack {
+                    Button(action: {
+                        hifispeakerButtonIsPressed.toggle()
+                    }, label: {
+                        Image(systemName: "hifispeaker")
+                            .foregroundColor(hifispeakerButtonIsPressed ? Color.green : Color.white)
+                            .font(.callout)
+                            .padding()
+                            .padding(.horizontal, 15)
+                        Spacer()
+                    })
+
+                    Button(action: {
+                        textBadgeButtonIsPressed.toggle()
+                    }, label: {
+                        Image(systemName: "text.badge.plus")
+                            .foregroundColor(textBadgeButtonIsPressed ? Color.green : Color.white)
+                            .font(.callout)
+                            .padding()
+                            .padding(.horizontal, 15)
+                    })
+                }
+                .padding(.bottom, 50)
+        
             }
+            
+
+            
         }
+    }
+    func formattedTime( second: Double) -> String {
+        let minute = Int(second) / 60
+        let remainingSeconds = Int(second) % 60
+        return String(format: "%02d:%02d", minute, remainingSeconds)
     }
 }
 
 #Preview {
-    MusicScreen(song: .init(title: "saasd", imageName: "müslüm1", color: .black))
+    MusicScreen(isShowing: .constant(true), song: .init(title: "saasd", imageName: "müslüm1", color: .black))
 }
